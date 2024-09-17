db01c525834d31574188a6a2a243ec54243fcf28
Jonas_Melo <jonashonorato4@gmail.com>
2024-09-17 20:15:12 -0300

    Update banco.py

--- flaskbackend/banco.py
+++ flaskbackend/banco.py
@@ -1,18 +1,15 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request,flash,redirect,url_for
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
-from .models import Usuario, Project
-from website import db
-from website.models import Usuario, Project
+from . import db 
+from .models import Usuario ,Project
 
-# Fetch all users from the database
+
+# Esta bosta não deu certo
+
 users = Usuario.query.all()
 
-# Print each user
-for user in users:
-    # Fetch all users from the database
-    users = Usuario.query.all()
-
-    # Print each user
-    for user in users:
-        print(user)
+for i in list(users):
+    print(i.id_user)
+    print(i.email)
+    print(i.password)
+    print("\n\n")
