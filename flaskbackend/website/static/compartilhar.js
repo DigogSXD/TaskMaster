41b6523be8d5328f3a21c79664dbf274df8a6a04
Diogo Borges de Moura <111078132+DigogSXD@users.noreply.github.com>
2024-10-01 14:04:45 -0300

    Delete flaskbackend directory

--- flaskbackend/website/static/compartilhar.js
+++
@@ -1,8 +0,0 @@
-function mostrarFormulario(projectId) {
-    var form = document.getElementById('compartilhar-form-' + projectId);
-    if (form.style.display === "none") {
-        form.style.display = "block";
-    } else {
-        form.style.display = "none";
-    }
-}
