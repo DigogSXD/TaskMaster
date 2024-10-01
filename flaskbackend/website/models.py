0303a0a1199ac22f972691f9ed20211c0bff199d
Diogo Borges de Moura <111078132+DigogSXD@users.noreply.github.com>
2024-10-01 14:06:06 -0300

    Add files via upload

---
+++ flaskbackend/website/models.py
@@ -0,0 +1,45 @@
+from . import db
+from flask_login import UserMixin
+from datetime import datetime
+
+# Modelo de Usuário
+class Usuario(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    
+    # Relacionamento com projetos (usando a tabela intermediária)
+    projects = db.relationship('Project', secondary='rel_usuario_project', backref='users')
+
+
+# Modelo de Projeto
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome_projeto = db.Column(db.String(100), nullable=False)
+    
+    # Você pode manter user_id se quiser identificar o criador do projeto
+    user_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    # Relacionamento reverso para facilitar o acesso ao criador
+    criador = db.relationship('Usuario', backref='projetos_criados')
+
+
+# Tabela intermediária entre usuários e projetos
+class RelUsuarioProject(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), primary_key=True)
+
+
+# A tabela da task
+class Task(db.Model):
+    id_task = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+
+    importance = db.Column(db.Integer, nullable=False)
+    ease = db.Column(db.Integer, nullable=False)
+    priority = db.Column(db.Integer, nullable=False)
+    completion_date = db.Column(db.Date, nullable=True)
+
+    # Nova coluna para o comentário
+    comment = db.Column(db.String(500), nullable=True)  # Comentário opcional
