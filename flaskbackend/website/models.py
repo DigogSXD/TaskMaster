4793e8e9321c9801bc7bda203001d883f372bbd6
Jonas Honorato Lima de Melo <jonashonorato4@gmail.com>
2024-10-22 11:35:27 -0300

    commitinho

--- flaskbackend/website/models.py
+++ flaskbackend/website/models.py
@@ -34,7 +34,7 @@ class Task(db.Model):
     id_task = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(255), nullable=False)
     status = db.Column(db.String(50), nullable=False)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
 
     importance = db.Column(db.Integer, nullable=False)
     ease = db.Column(db.Integer, nullable=False)
@@ -44,11 +44,13 @@ class Task(db.Model):
     # Nova coluna para o comentário
     comment = db.Column(db.String(500), nullable=True)  # Comentário opcional
 
+    # Relacionamento com ChecklistItem
+    checklist_items = db.relationship('ChecklistItem', backref='task', cascade='all, delete-orphan')
+
+
 # Modelo de ChecklistItem
 class ChecklistItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_id = db.Column(db.Integer, db.ForeignKey('task.id_task'), nullable=False)
     description = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
-
-    task = db.relationship('Task', backref='checklist_items')
